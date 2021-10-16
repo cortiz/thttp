e0b13bda5b5650fc7838143103f5d495f9d577f7
Carlos Ortiz <chrono.dark@gmail.com>
2021-10-15 20:09:56 -0600

    Added Kong Flags

--- thttp/main.go
+++ thttp/main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/alecthomas/kong"
 )
 
 var CLI struct {
-	Flag []string
+	File         []string `short:"f" help:"Files to execute." optional:"true" type:"existingfile"`
+	Dir          []string `short:"d" help:"Executes all the files in the given directories" optional:"true" type:"existingdir"`
+	Properties   string   `short:"p" help:"Properties file to be use can be yaml, json or property file" optional:"true"`
+	Repeat       int      `short:"r" help:"Repeats the requests the given times" optional:"true"`
+	HeaderOnly   bool     `short:"I" help:"Print only the headers" optional:"true"`
+	WithHeader   bool     `short:"i" help:"Print the response with the headers" optional:"true"`
+	Output       string   `short:"o" help:"Write the output to a file" optional:"true"`
+	OutputFolder string   `short:"f" help:"Files to execute." optional:"true" type:"existingdir"`
+	IgnoreSSL    bool     `short:"k" help:"Ignore SSL Validation" optional:"true"`
+	Follow       bool     `short:"l" help:"Follow Redirects" optional:"true"`
+	Plugin       []string `help:"Plugins to Load" optional:"true" type:"existingfile"`
 }
 
 func main() {
 	ctx := kong.Parse(&CLI)
-	fmt.Println(ctx.Command())
-	for i := range CLI.Flag {
-		fmt.Printf("%d %s \n", i, CLI.Flag[i])
+	if len(CLI.Dir) >= 0 && len(CLI.File) >= 0 {
+		fmt.Println("Either File or Directory is require")
+		err := ctx.PrintUsage(true)
+		if err != nil {
+			fmt.Println("Error")
+		}
+		os.Exit(1)
 	}
+
 	// request, err := request.NewRequest("../test-requests/simpleRequest.txt")
 	// if err != nil {
 	// 	fmt.Println(err)
